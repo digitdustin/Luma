8ac481169ce8c361b4eb631bda9bcc310e1f2e78
Dustin Karp <dustinkarp52@gmail.com>
2020-12-18 20:03:45 -0500

    added more swatches

--- src/swatches.js
+++ src/swatches.js
@@ -2,15 +2,15 @@ const currentSwatch = document.createElement('div');
 currentSwatch.id = 'current-swatch';
 
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 16; i++) {
     const newSwatch = document.createElement('div');
     newSwatch.classList.add('swatch');
-    newSwatch.style.backgroundColor = `hsl(${i / 12 * 360}, ${(i / 12 * 20) + 50}%, ${(i / 12 * 20) + 40}%)`
+    newSwatch.style.backgroundColor = `hsl(${i / 16 * 360}, ${(i / 16 * 20) + 50}%, ${(i / 16 * 20) + 40}%)`
     newSwatch.addEventListener('click', () => {
         //clicking a color will change sliders and big swatch
-        document.getElementById('hue-slider').value = i / 12 * 360;
-        document.getElementById('saturation-slider').value = (i / 12 * 20) + 50;
-        document.getElementById('brightness-slider').value = (i / 12 * 20) + 40;
+        document.getElementById('hue-slider').value = i / 16 * 360;
+        document.getElementById('saturation-slider').value = (i / 16 * 20) + 50;
+        document.getElementById('brightness-slider').value = (i / 16 * 20) + 40;
         document.getElementById('current-swatch').style.backgroundColor = `hsl(${hueSlider.value}, ${saturationSlider.value}%, ${brightnessSlider.value}%)`;
     })
     document.getElementById('swatches').appendChild(newSwatch);
