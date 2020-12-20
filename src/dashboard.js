ea0c0f3b074e67a52e6d236e7ec97e50a03402f0
Dustin Karp <dustinkarp52@gmail.com>
2020-12-20 01:09:54 -0500

    testing for sample lights

--- src/dashboard.js
+++ src/dashboard.js
@@ -5,22 +5,45 @@ var client = new LifxClient();
 
 //initialize light discovery client
 client.init();
-console.log('client destroyed');
+console.log('dashboard client created');
 
-const zonesBtn = document.getElementById('zones-button');
-const dashboardBtn = document.getElementById('dashboard-button');
-zonesBtn.addEventListener('click', () => {
-    client.stopDiscovery();
-    client.stopSendingProcess();
-    console.log('dashboard client paused');
-})
+const lightSelect = document.getElementById('light-select');
+var option = document.createElement('option');
+option.innerText = 'Select';
+lightSelect.add(option);
 
-dashboardBtn.addEventListener('click', () => {
-    console.log('hello')
-    client.startDiscovery();
-    client.startSendingProcess();
-    console.log('dashboard client resumed');
-})
+//FOR TESTING-------
+
+var option2 = document.createElement('option');
+option2.innerText = 'sample light';
+lightSelect.add(option2);
+
+var option3 = document.createElement('option');
+option3.innerText = 'sample light 2';
+lightSelect.add(option3);
+
+var lightName2 = document.createElement("div");
+        lightName2.classList.add('light-name');
+        lightName2.innerText = 'sample light 1'.toUpperCase();
+        lightName2.addEventListener('click', () => {
+            //on list light click, change color of div and change all sliders/swatches
+            var lightsInList = document.getElementsByClassName('light-name');
+            for (let i = 0; i < lightsInList.length; i++) {
+                lightsInList[i].style.backgroundColor = 'transparent';
+                lightsInList[i].style.color = 'rgb(209, 209, 209)';
+            }
+            lightName2.style.backgroundColor = '#283136';
+            lightName2.style.color = 'white';
+        });
+        document.getElementById('light-list').appendChild(lightName2);
+
+//------------------
+
+lightSelect.addEventListener('change', (e) => {
+    if (e.target.value.innerText != 'Select') {
+        option.disabled = true;
+    }
+});
 
 const lightContainer = document.getElementById('light-list');
 const hueSlider = document.getElementById('hue-slider');
@@ -75,6 +98,14 @@ client.on('light-new', (light) => {
             throw error;
         }
         console.log(data);
+        
+        //add light to zones dropdown
+        
+        var lightOption = document.createElement('option');
+        lightOption.innerText = data;
+        lightSelect.add(lightOption);
+
+        //add light to light list
         const lightName = document.createElement("div");
         lightName.classList.add('light-name');
         lightName.innerText = data.toUpperCase();
